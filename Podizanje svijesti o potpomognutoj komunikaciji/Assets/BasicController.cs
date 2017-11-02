0d95e0ba1c26698ada0f18d02e83d83c44c26454
Dinko[cURL] <dinkochess@gmail.com>
2017-11-02 15:38:57 +0100

    First level basics setup

---
+++ Podizanje svijesti o potpomognutoj komunikaciji/Assets/BasicController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicController : MonoBehaviour {
+
+    public GameObject character;
+    public float speed = 3.0f;
+    public Camera mainCamera;
+    private float offset;
+
+    private void Start() {
+        offset = mainCamera.transform.position.z - character.transform.position.z;
+        mainCamera.transform.position = new Vector3(character.transform.position.x, character.transform.position.y, offset);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        MoveCharacter();
+    }
+
+    private void LateUpdate() {
+        mainCamera.transform.position = new Vector3(character.transform.position.x, character.transform.position.y, offset);
+    }
+
+    void MoveCharacter() {
+
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            character.transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            character.transform.position += new Vector3(0, -speed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            character.transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            character.transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+
+        
+    }
+}
