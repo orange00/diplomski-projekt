a6199501b37db4892763dac58991d7e53bcd01cb
Dinko[cURL] <dinkochess@gmail.com>
2017-12-18 17:43:03 +0100

    First level about 70% done

--- Podizanje svijesti o potpomognutoj komunikaciji/Assets/BasicController.cs
+++ Podizanje svijesti o potpomognutoj komunikaciji/Assets/BasicController.cs
@@ -4,42 +4,77 @@ using UnityEngine;
 
 public class BasicController : MonoBehaviour {
 
-    public GameObject character;
-    public float speed = 3.0f;
+    private Transform characterTransform;
+    private Rigidbody2D charRigidbody;
+    public float speed = 3f;
     public Camera mainCamera;
     private float offset;
+    private SpriteRenderer spriteRenderer;
+    private Animator characterAnimator;
+    private float moveX, moveY;
+
+    // True -> right, False -> left
+    private bool checkDirection = true;
 
     private void Start() {
-        offset = mainCamera.transform.position.z - character.transform.position.z;
-        mainCamera.transform.position = new Vector3(character.transform.position.x, character.transform.position.y, offset);
+        charRigidbody = GetComponent<Rigidbody2D>();
+        characterTransform = GetComponent<Transform>();
+        offset = mainCamera.transform.position.z - characterTransform.position.z;
+        //mainCamera.transform.position = new Vector3(character.transform.position.x, character.transform.position.y, offset);
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        characterAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update () {
+
+        moveX = Input.GetAxisRaw("Horizontal");
+        moveY = Input.GetAxisRaw("Vertical");
+
+        if (moveX == 0 && moveY == 0) {
+
+            //characterAnimator.Play("IdleStand");
+            characterAnimator.SetBool("isMoving", false);
+        }
+
+        if (moveX != 0) {
+            characterAnimator.SetBool("isMoving", true);
+        }
+        if (moveY != 0) {
+            characterAnimator.SetBool("isMoving", true);
+        }
+    }
+
+    //private void LateUpdate() {
+        
+    //    mainCamera.transform.position = new Vector3(character.transform.position.x, character.transform.position.y, offset);
+    //}
+
+    private void FixedUpdate() {
         MoveCharacter();
     }
 
-    private void LateUpdate() {
-        mainCamera.transform.position = new Vector3(character.transform.position.x, character.transform.position.y, offset);
+    void MoveCharacter() {
+        
+
+        if (moveX != 0) {
+            CheckDirection(moveX);
+            characterTransform.position += new Vector3(moveX * speed * Time.deltaTime, 0, 0);
+            //characterAnimator.SetBool("isMoving", true);
+        }
+        if (moveY != 0) {
+            characterTransform.position += new Vector3(0, moveY * speed * Time.deltaTime, 0);
+            //characterAnimator.SetBool("isMoving", true);
+        }      
     }
 
-    void MoveCharacter() {
-
-        if (Input.GetKey(KeyCode.UpArrow)) {
-            character.transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+    void CheckDirection(float moveValue) {
+        if(moveValue < 0.0f) {
+            spriteRenderer.flipX = true;
+            checkDirection = false;
+        } else {
+            spriteRenderer.flipX = false;
+            checkDirection = true;
         }
-
-        if (Input.GetKey(KeyCode.DownArrow)) {
-            character.transform.position += new Vector3(0, -speed * Time.deltaTime, 0);
-        }
-        if (Input.GetKey(KeyCode.LeftArrow)) {
-            character.transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
-        }
-
-        if (Input.GetKey(KeyCode.RightArrow)) {
-            character.transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
-        }
-
-        
     }
 }
---
+++ Podizanje svijesti o potpomognutoj komunikaciji/Assets/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public Sprite soundOff, soundOn;
+
+	public void ChangeScene(string name) {
+		SceneManager.LoadScene(name);
+	}
+
+    public void ChangeVolume(bool enableVolume) {
+        if (!enableVolume) {
+
+        } else {
+
+        }
+    }
+}
