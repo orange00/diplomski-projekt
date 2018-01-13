468f59a63b8d3ccc35aae4d9a143b6d31677a449
Mate <mate.simovic@fer.hr>
2018-01-13 16:40:05 +0100

    Sad stvarno najnovija verzija :D

---
+++ Podizanje svijesti o potpomognutoj komunikaciji/Assets/OpenLink.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour {
+    public void OpenFacebookHyperlink() {
+        Application.OpenURL("https://www.facebook.com/ictaac");
+    }
+    public void OpenICTAACHyperlink()
+    {
+        Application.OpenURL("http://www.ict-aac.hr/index.php/hr/");
+    }
+}
