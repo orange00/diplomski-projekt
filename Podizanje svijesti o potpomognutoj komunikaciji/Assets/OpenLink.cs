76d29cf51c1a7ebf44d82a52f2c2d40cd01501fb
DESKTOP-7AN2ML8\Mela <karmela.sliskovic@fer.hr>
2018-02-09 13:09:28 +0100

    Isprogramirano da se linkovi sa završne scene otvaraju u novim tabovima

--- Podizanje svijesti o potpomognutoj komunikaciji/Assets/OpenLink.cs
+++ Podizanje svijesti o potpomognutoj komunikaciji/Assets/OpenLink.cs
@@ -6,10 +6,21 @@ using UnityEngine;
 
 public class OpenLink : MonoBehaviour {
     public void OpenFacebookHyperlink() {
-        Application.OpenURL("https://www.facebook.com/ictaac");
+        if (Application.platform == RuntimePlatform.WebGLPlayer)
+        {
+            //Application.OpenURL("https://www.facebook.com/ictaac");
+            //Application.ExternalEval("window.open(\"https://www.facebook.com/ictaac",\"_blank")");
+            Application.ExternalEval("window.open(\"https://www.facebook.com/ictaac\")");
+            return;
+        }
     }
     public void OpenICTAACHyperlink()
     {
-        Application.OpenURL("http://www.ict-aac.hr/index.php/hr/");
+        if (Application.platform == RuntimePlatform.WebGLPlayer)
+        {
+            //Application.OpenURL("http://www.ict-aac.hr/index.php/hr/");
+            Application.ExternalEval("window.open(\"http://www.ict-aac.hr/index.php/hr\")");
+            return;
+        }
     }
 }
